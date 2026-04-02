a900713842d664ac4c191190f1979dee319a7a41
Daniel <Daniel@DWVT>
2026-04-02 05:12:50 -0300

    Add-Repositories

---
+++ GestaoRH_API/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,56 @@
+﻿using GestaoRH_API.Data;
+using GestaoRH_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoRH_API.Repositories
+{
+    public class FuncionarioRepository : IFuncionarioRepository
+    {
+        private readonly AppDbContext _context;
+
+        public FuncionarioRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Funcionario>> GetAllListAsync()
+        {
+            return await _context.Funcionarios.ToListAsync();
+        }
+        public async Task<Funcionario> GetByIdAsync(int id)
+        {
+            return await _context.Funcionarios.FindAsync(id);
+
+        }
+        public async Task<Funcionario> CreateAsync(Funcionario funcionario)
+        {
+            funcionario.DataContratacao = DateTime.Now;
+
+            _context.Funcionarios.Add(funcionario);
+            await _context.SaveChangesAsync();
+            return funcionario;
+        }
+        public async Task<Funcionario> UpdateAsync(int id, Funcionario funcionario)
+        {
+            var funcionarioEncontrado = await _context.Funcionarios.FindAsync(id);
+
+            funcionarioEncontrado.Nome = funcionario.Nome;
+            funcionarioEncontrado.Cargo = funcionario.Cargo;
+            funcionarioEncontrado.Salario = funcionario.Salario;
+            funcionarioEncontrado.DataContratacao = funcionario.DataContratacao;
+
+            await _context.SaveChangesAsync();
+            return funcionario;
+
+
+        }
+        public async Task<Funcionario> DeleteAsync(int id)
+        {
+            var funcionarioEncontrado = await _context.Funcionarios.FindAsync(id);
+            _context.Funcionarios.Remove(funcionarioEncontrado);
+            await _context.SaveChangesAsync();
+            return funcionarioEncontrado;
+        }
+
+    }
+}
---
+++ GestaoRH_API/Repositories/IFuncionarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using GestaoRH_API.Models;
+
+namespace GestaoRH_API.Repositories
+{
+    public interface IFuncionarioRepository
+    {
+        Task<IEnumerable<Funcionario>> GetAllListAsync();
+        Task<Funcionario> GetByIdAsync(int id);
+        Task<Funcionario> CreateAsync(Funcionario funcionario);
+        Task<Funcionario> UpdateAsync(int id, Funcionario funcionario);
+        Task<Funcionario> DeleteAsync(int id);
+
+
+    }
+}
