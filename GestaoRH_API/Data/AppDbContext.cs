d6a9c558b6d211aa8594287880f72353695c2e80
Daniel <Daniel@DWVT>
2026-04-02 04:54:34 -0300

    Add-EF-and-AppDbContext

---
+++ GestaoRH_API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GestaoRH_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+    }
+}
